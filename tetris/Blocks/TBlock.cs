3b4a1c691b70cb1c0619632a0103e7bf646d09c4
Vicki <s6p1j@students.cs.ubc.ca>
2023-06-21 14:30:14 -0700

    board

---
+++ tetris/Blocks/TBlock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tetris.Blocks
+{
+    public class TBlock : Block
+    {
+        /*
+         positions
+
+        # default, right, down, left
+
+        OXO
+        XXX
+        OOO
+
+        OXO
+        OXX
+        OXO
+
+        OOO
+        XXX
+        OXO
+
+        OXO
+        XXO
+        OXO
+         
+         */
+
+
+
+
+
+    }
+}
